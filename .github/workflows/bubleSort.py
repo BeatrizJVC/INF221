a41591fcf832adde0292958599d30f8db2e23e40
BeatrizJVC <117328913+BeatrizJVC@users.noreply.github.com>
2024-09-09 23:56:52 -0300

    Delete .github/workflows directory

--- .github/workflows/bubleSort.py
+++
@@ -1,8 +0,0 @@
-def bubble_sort_matriz(matriz):
-    for row in matriz:
-        n = len(row)
-        for i in range(n):
-            for j in range(0, n-i-1):
-                if row[j] > row[j+1]:
-                    row[j], row[j+1] = row[j+1], row[j]
-    return matriz
