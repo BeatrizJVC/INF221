3a8b30865b6e4907157bb1b5815b77fc74cc8a1b
BeatrizJVC <117328913+BeatrizJVC@users.noreply.github.com>
2024-09-11 00:41:12 -0300

    Update MatrizAleatoria.py

--- Ordenamiento/MatrizAleatoria.py
+++ Ordenamiento/MatrizAleatoria.py
@@ -1,19 +1,13 @@
 import random
 
-def matriz_aleatoria(filas, columnas, valor_min, valor_max):
-    return [[random.randint(valor_min, valor_max) for _ in range(columnas)] for _ in range(filas)]
-
-filas1=int(input("Ingrese el numero de filas: "))
-columnas1 =int(input("Ingrese el numero de columnas: "))
-valor_min1=int(input("Ingrese el valor minimo: "))
-valor_max1=int(input("Ingrese el valor maximo: "))
-
-lista1 = matriz_aleatoria(filas1,columnas1,valor_min1,valor_max1)
-
-def guardar(lista_a, nombre_archivo):
-    with open(nombre_archivo, 'w') as archivo:
+def matriz_aleatoria():
+    filas=int(input("Ingrese el numero de filas: "))
+    columnas =int(input("Ingrese el numero de columnas: "))
+    valor_min=int(input("Ingrese el valor minimo: "))
+    valor_max=int(input("Ingrese el valor maximo: "))
+    lista = [[random.randint(valor_min, valor_max) for _ in range(columnas)] for _ in range(filas)]
+    with open("Matriz.txt", 'w') as archivo:
         archivo.write("Matriz =\n")
-        for elemento1 in lista_a:
+        for elemento1 in lista:
             archivo.write(f"{elemento1}\n")
-
-guardar(lista1,"Matriz.txt")
+    return lista
