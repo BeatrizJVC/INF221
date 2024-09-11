cc7f4291dab408e412ee208fc667b74b60d9dc89
BeatrizJVC <117328913+BeatrizJVC@users.noreply.github.com>
2024-09-10 21:51:41 -0300

    Create InsertionSort.py

---
+++ Ordenamiento/InsertionSort.py
@@ -0,0 +1,10 @@
+def insertion_sort(matriz):
+    for row in matriz:
+        for i in range(1, len(row)):
+            key = row[i]
+            j = i - 1
+            while j >= 0 and key < row[j]:
+                row[j + 1] = row[j]
+                j -= 1
+            row[j + 1] = key
+    return matriz
