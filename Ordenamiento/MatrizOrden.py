4cd0f7b0df1e2c12d12a47142e8aaa5b5761b47e
BeatrizJVC <117328913+BeatrizJVC@users.noreply.github.com>
2024-09-11 01:09:13 -0300

    Update MatrizOrden.py

--- Ordenamiento/MatrizOrden.py
+++ Ordenamiento/MatrizOrden.py
@@ -1,35 +1,31 @@
 import numpy as np
 
-def matriz_orden(filas, columnas, tipo_orden='aleatorio'):
+def matriz_orden():
+    filas=int(input("Ingrese el numero de filas: "))
+    columnas =int(input("Ingrese el numero de columnas: "))
+    tipo_orden=int(input("Ingrese el tipo de ordenación 1 ('1.aleatorio', '2.completamente_ordenado', '3.semi_ordenado', '4.parcialmente_ordenado'): "))
+
     matriz = np.random.randint(0, 100, size=(filas, columnas))
     
     if tipo_orden == 1:
-        return matriz
+        lista=matriz
     elif tipo_orden == 2:
-        return np.sort(matriz, axis=1)
+        lista = np.sort(matriz, axis=1)
     elif tipo_orden == 3:
         num_filas_a_ordenar = filas // 2
         for i in range(num_filas_a_ordenar):
             matriz[i] = np.sort(matriz[i])
-        return matriz
+        lista = matriz
     elif tipo_orden == 4:
         for fila in matriz:
             mitad = len(fila) // 2
             fila[:mitad] = np.sort(fila[:mitad])
-        return matriz
+        lista = matriz
     else:
         raise ValueError("Tipo de ordenación no reconocido")
-
-filas1=int(input("Ingrese el numero de filas: "))
-columnas1 =int(input("Ingrese el numero de columnas: "))
-orden1=int(input("Ingrese el tipo de ordenación 1 ('1.aleatorio', '2.completamente_ordenado', '3.semi_ordenado', '4.parcialmente_ordenado'): "))
-
-lista1 = matriz_orden(filas1,columnas1,orden1)
-
-def guardar(lista_a, nombre_archivo):
-    with open(nombre_archivo, 'w') as archivo:
+        
+    with open("Matriz.txt", 'w') as archivo:
         archivo.write("Matriz =\n")
-        for elemento1 in lista_a:
+        for elemento1 in lista:
             archivo.write(f"{elemento1}\n")
-
-guardar(lista1,"Matriz.txt")
+    return lista
