484bea74391d957e8eea173afb742c6d96c091ef
BeatrizJVC <117328913+BeatrizJVC@users.noreply.github.com>
2024-09-11 01:52:35 -0300

    Update MatricesOrden.py

--- Multiplicacion/MatricesOrden.py
+++ Multiplicacion/MatricesOrden.py
@@ -1,38 +1,42 @@
 import numpy as np
 
-def matriz_orden(filas, columnas, tipo_orden='aleatorio'):
+def matriz_orden():
+    filas=int(input("Ingrese el numero de filas: "))
+    columnas =int(input("Ingrese el numero de columnas: "))
+    tipo_orden=int(input("Ingrese el tipo de ordenación (1. Aleatorio, 2. Completamente ordenado, 3. Semi ordenado, 4. Parcialmente ordenado): "))
+
     matriz = np.random.randint(0, 100, size=(filas, columnas))
     
     if tipo_orden == 1:
-        return matriz
+        lista = matriz
     elif tipo_orden == 2:
-        return np.sort(matriz, axis=1)
+        lista = np.sort(matriz, axis=1)
     elif tipo_orden == 3:
         num_filas_a_ordenar = filas // 2
         for i in range(num_filas_a_ordenar):
             matriz[i] = np.sort(matriz[i])
-        return matriz
+        lista = matriz
     elif tipo_orden == 4:
         for fila in matriz:
             mitad = len(fila) // 2
             fila[:mitad] = np.sort(fila[:mitad])
-        return matriz
+        lista = matriz
     else:
         raise ValueError("Tipo de ordenación no reconocido")
+    return lista
 
-filas1=int(input("Ingrese el numero de filas de la primera matriz: "))
-columnas1 =int(input("Ingrese el numero de columnas de la primera matriz: "))
-orden1=int(input("Ingrese el tipo de ordenación 1 ('1.aleatorio', '2.completamente_ordenado', '3.semi_ordenado', '4.parcialmente_ordenado'): "))
+def crear_matrices():
+    print("Caracteristicas matriz 1:")
+    lista1 = matriz_orden()
+    print("\nCaracteristicas matriz 2:")
+    lista2 = matriz_orden()
+    return [lista1, lista2]
 
-filas2=int(input("\nIngrese el numero de filas de la segunda matriz: "))
-columnas2 = int(input("Ingrese el numero de columnas de la segunda matriz: "))
-orden2=int(input("Ingrese el tipo de ordenación 2 ('1.aleatorio', '2.completamente_ordenado', '3.semi_ordenado', '4.parcialmente_ordenado'): "))
-
-lista1 = matriz_orden(filas1,columnas1,orden1)
-lista2 = matriz_orden(filas2,columnas2,orden2)
-
-def guardar(lista_a, lista_b, nombre_archivo):
-    with open(nombre_archivo, 'w') as archivo:
+def matrices_orden():
+    listas=crear_matrices()
+    lista_a = listas[0]
+    lista_b = listas[1]
+    with open("Matrices.txt", 'w') as archivo:
         archivo.write("Matriz 1 =\n")
         for elemento1 in lista_a:
             archivo.write(f"{elemento1}\n")
@@ -40,5 +44,4 @@ def guardar(lista_a, lista_b, nombre_archivo):
         archivo.write("\n\nMatriz 2 =\n")
         for elemento2 in lista_b:
             archivo.write(f"{elemento2}\n")
-
-guardar(lista1,lista2,"Matrices.txt")
+    return listas
